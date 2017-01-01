abc1f61fe591be1f5496a30749e601b882c215b2
alanzfduan <lzcgwushuang@163.com>
2017-01-01 08:57:27 -0800

    A wishful implementation for template

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+template<typename T,Name X>
+class Struct
+{
+    T X;
+};
+
+using my_struct = Struct<int,'id'>;
+
+int main(int argc,char *argv[])
+{
+    my_struct dd;
+    dd.id=1;
+    cout<<dd.id<<endl;
+    return 0;
+}
