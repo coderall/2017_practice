86b792e775259df2dc5e33b36cf5de09513e46e1
alanzfduan <lzcgwushuang@163.com>
2017-04-13 06:01:33 -0700

    a simple for loop example

---
+++ shell/for.sh
@@ -0,0 +1,11 @@
+## File Name:    for.sh
+## Author:     alan
+## mail:       lzcgwushuang@163.com
+## Created Time: Thu 13 Apr 2017 02:59:12 AM PDT
+
+#!/bin/bash
+
+for (( i=0;i<5;i++ ));do
+    echo "i is $i"
+done
+
