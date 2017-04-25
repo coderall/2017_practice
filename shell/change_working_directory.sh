31edab21c2bf538c8e05030e7e59b2c801b6606b
alanzfduan <lzcgwushuang@163.com>
2017-04-25 08:18:30 -0700

    change a shell working directory

---
+++ shell/change_working_directory.sh
@@ -0,0 +1,24 @@
+# File Name:    change_working_directory.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Tue 25 Apr 2017 08:13:05 AM PDT
+
+#!/bin/bash
+
+homedir=~/
+
+pushd $homedir
+
+echo "$(pwd)"
+
+# do something in the new dir
+
+touch test_file
+
+popd
+
+# go back the init dir
+
+echo "$(pwd)"
+
+exit 0
