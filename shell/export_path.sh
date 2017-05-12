7ca1ce11a2578f26effe98bd40de89caed4fbe3e
alanzfduan <lzcgwushuang@163.com>
2017-05-12 03:41:05 -0700

    export environment variable

---
+++ shell/export_path.sh
@@ -0,0 +1,9 @@
+# File Name:    export_path.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Fri 12 May 2017 03:39:13 AM PDT
+
+#!/bin/bash
+
+###   use the colon to seperate all path   ###
+export PATH="$PAH:/some/dir/of/bin"
