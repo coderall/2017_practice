c7f73c8b8e24eb018cd2c303d625e878494ae4e4
alanzfduan <lzcgwushuang@163.com>
2017-05-21 04:30:18 -0700

    add gdb show script

---
+++ shell/gdb.sh
@@ -0,0 +1,16 @@
+#/*************************************************************************
+# File Name:    gdb.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Sun 21 May 2017 04:27:24 AM PDT
+#************************************************************************/
+
+#!/bin/bash
+
+# this is a simple gdb use case show
+
+# No.1 debug the core file
+
+gdb -c corefile binfile
+
+
