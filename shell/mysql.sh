70174d52d1e938fcc58b772af2866e3a15af1cc7
alanzfduan <lzcgwushuang@163.com>
2017-05-22 00:47:31 -0700

    add mysql example script

---
+++ shell/mysql.sh
@@ -0,0 +1,28 @@
+# File Name:    mysql.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Mon 22 May 2017 12:43:47 AM PDT
+
+#!/bin/bash
+
+# this is a simple mysql userfull command script
+
+# No.1 show the table detail
+
+show create table tbTest;
+
+# No.2 show the table index info
+
+show index from database.tablename
+
+# No.3 show table status
+
+show table status from database
+
+# No.4 look the select plan
+
+explain sql_statement
+
+# No.5 look the variable value
+
+show variables like '%var%'
