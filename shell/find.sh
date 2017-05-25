6ed71dca9d5a57553d493e500710d781a26ab4a1
alanzfduan <lzcgwushuang@163.com>
2017-05-24 21:38:35 -0700

    add find command example

---
+++ shell/find.sh
@@ -0,0 +1,23 @@
+# File Name:    find.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Wed 24 May 2017 09:26:17 PM PDT
+
+#!/bin/bash
+
+# this is a find command show script
+
+# No.1 find the file with name
+
+find / -name filename -type f || find / -name dirname -type d     and so on...
+
+# No.2 find the file last modified n days(n*24) ago
+
+find / -name filename -mtime n -type f
+
+# No.3 find and exec some command 
+# NOTICE: the space before the back slash is neccssary
+
+find / -name filename -type f -exec du -s -h '{}' \;
+find / -name filename -type f -exec file '{}' \;
+find / -name filename -type f -exec rm '{}' \;
