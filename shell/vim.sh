803da03dd49ccc2fd6d0aff80eeae74166f594b2
alanzfduan <lzcgwushuang@163.com>
2017-05-27 04:13:02 -0700

    add vim example script

---
+++ shell/vim.sh
@@ -0,0 +1,13 @@
+# File Name:    vim.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Fri 26 May 2017 11:08:06 PM PDT
+
+#!/bin/bash
+
+# this is vim example script, it could be numerous
+
+# No.1 vim +'some command'
+# you will never open the txt,it's a hack
+vim +q test.txt
+
