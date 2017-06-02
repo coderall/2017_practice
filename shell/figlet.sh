dbb87b9d8ba1db4e4ffe71ce014a1d94b029cd7d
alanzfduan <lzcgwushuang@163.com>
2017-06-01 22:03:38 -0700

    add funny figlet command

---
+++ shell/figlet.sh
@@ -0,0 +1,14 @@
+/*************************************************************************
+# File Name:    figlet.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Thu 01 Jun 2017 10:02:38 PM PDT
+ ************************************************************************/
+
+#!/bin/bash
+
+# a funny text command
+
+figlet i love you
+
+# you will see a funny pic-text
