add1ddca4ffd4b26b6e962704d3b928ac4f4d05f
alanzfduan <lzcgwushuang@163.com>
2017-06-01 21:57:20 -0700

    add funny sl command

---
+++ shell/sl.sh
@@ -0,0 +1,14 @@
+/*************************************************************************
+# File Name:    sl.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Thu 01 Jun 2017 09:56:18 PM PDT
+ ************************************************************************/
+
+#!/bin/bash
+
+# this is a funny shell command 
+
+sl
+
+# if you want run this command , please apt-get install sl
