45888eef797d1bc43dc533aaace1e7f68e9e6b4a
alanzfduan <lzcgwushuang@163.com>
2017-06-03 08:24:15 -0700

    add the free comand

---
+++ shell/free.sh
@@ -0,0 +1,20 @@
+# File Name:    free.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Sat 03 Jun 2017 08:21:47 AM PDT
+
+#!/bin/bash
+
+# free command example
+
+# No.1 get the mem status
+
+free
+
+# No.2 get the num with K or M or G
+
+free -k or free -m or free -g
+
+# No.3 get the total info show
+
+free -gt
