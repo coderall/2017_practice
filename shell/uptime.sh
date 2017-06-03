a9f99cc54027d41ec30d8f586941e9e5680b39f5
alanzfduan <lzcgwushuang@163.com>
2017-06-03 08:27:08 -0700

    add uptime command

---
+++ shell/uptime.sh
@@ -0,0 +1,12 @@
+# File Name:    uptime.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Sat 03 Jun 2017 08:26:03 AM PDT
+
+#!/bin/bash
+
+# the uptime command can get the CPU load info from user login
+
+# No.1 get the average load info
+
+uptime
