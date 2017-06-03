bea036970ed95095b0af8127661119aed2537f03
alanzfduan <lzcgwushuang@163.com>
2017-06-03 07:24:29 -0700

    add id example command

---
+++ shell/id.sh
@@ -0,0 +1,16 @@
+# File Name:    id.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Sat 03 Jun 2017 07:22:54 AM PDT
+
+#!/bin/bash
+
+# the id command example
+
+# No.1 get the current user id
+id -u 
+
+# No.2 get the current user group id
+
+id -g
+
