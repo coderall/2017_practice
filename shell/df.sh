1406fed08b5668257f8ccd5abea3cd1dbbe69274
alanzfduan <lzcgwushuang@163.com>
2017-06-13 23:55:49 -0700

    add df command

---
+++ shell/df.sh
@@ -0,0 +1,10 @@
+# File Name:    df.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Tue 13 Jun 2017 11:54:43 PM PDT
+
+#!/bin/bash
+
+# No.1 get the disk space info and the mount info
+
+du -ah
