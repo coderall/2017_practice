ef20664406e5432294778eac8b364ca3d2401051
alanzfduan <lzcgwushuang@163.com>
2017-07-04 03:05:09 -0700

    nmon command

---
+++ shell/nmon.sh
@@ -0,0 +1,10 @@
+# File Name:    nmon.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Tue 04 Jul 2017 03:03:00 AM PDT
+
+#!/bin/bash
+
+# nmon is a integreted command that can monitor CPU Mem I/O network and some profile detail ,it has a friendly GUI
+
+nmon
