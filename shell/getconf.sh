3cb4dfe43b17f429fc5e4968da77f127e546c50c
alanzfduan <lzcgwushuang@163.com>
2017-07-04 10:28:38 -0700

    add getconfi command

---
+++ shell/getconf.sh
@@ -0,0 +1,10 @@
+# File Name:    getconf.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Tue 04 Jul 2017 10:26:14 AM PDT
+
+#!/bin/bash
+
+# getconf can fetch system variable
+getconf -a
+# this can fetch cache size,cache line size ,the size of int and so on
