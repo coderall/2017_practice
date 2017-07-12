649efb8af85982621f8cac2554cf26d062d572b7
alanzfduan <lzcgwushuang@163.com>
2017-07-12 03:03:38 -0700

    add some built-in shell command

---
+++ shell/shell.sh
@@ -0,0 +1,22 @@
+# File Name:    shell.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Wed 12 Jul 2017 02:56:13 AM PDT
+
+#!/bin/bash
+
+# this file introduce some built-in shell command
+
+# No.1 !! to execute the last command
+!!
+
+# No.2 !vim to execute the last command begining with 'vim'
+!vim
+
+# No.3 clear the command line from current position to the top left
+ctrl+d   # and ctrl+a ctrl+e
+ctrl+k   # clear the command line from current position to the top right
+
+# No.4 get the last command's last parameter
+echo _$
+
