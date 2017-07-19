bd3e2f59abf9d0ba1e1af8130afddf7c7dc24485
alanzfduan <lzcgwushuang@163.com>
2017-07-19 10:58:35 -0700

    add pass awk result to shell variable

--- shell/awk.sh
+++ shell/awk.sh
@@ -16,3 +16,10 @@ awk -F ' ' '/pattern/{print $1 $2}'
 # No.3 the last parameter the $NF stands for the last parameter
 awk -F ' ' '/pattern/{print $NF}'
 
+# No.4 pass the awk result to shell variable
+# this is a trick 
+arr=($(echo 'a b c'|awk -F ' ' '{printf("%c %c %c",$1,$2,$3)}'))
+test1=${arr[0]}
+test2=${arr[1]}
+test3=${arr[2]}
+# now you can execute your own logic by this variable
