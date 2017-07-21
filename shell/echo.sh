2585ed164bf59318c51e557f3ac9d3f306ca5338
alanzfduan <lzcgwushuang@163.com>
2017-07-21 07:27:38 -0700

    add echo command

---
+++ shell/echo.sh
@@ -0,0 +1,14 @@
+# File Name:    echo.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Fri 21 Jul 2017 07:24:31 AM PDT
+
+#!/bin/bash
+
+# echo is a simple command,but it's useful
+
+# No.1 print the line without '\n'
+echo -n "hello"
+
+# No.2 enable interpretation of the following backslash-escaped characters
+echo -e 'a \t b \t c'
