03b5cf8325cefd8d51a645c7186c9e457c39deaa
alanzfduan <lzcgwushuang@163.com>
2017-08-02 08:14:45 -0700

    add slabtop command

---
+++ shell/slabtop.sh
@@ -0,0 +1,13 @@
+# File Name:    slabtop.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Wed 02 Aug 2017 08:12:10 AM PDT
+
+#!/bin/bash
+
+# slabtop is a tool to show kernel slab info
+# here we should know that the result contain user space and kernel space
+
+slabtop
+
+# cat /proc/meminfo is useful too
