5e898ef83733408b4818e2850b9703e007e52c3a
alanzfduan <lzcgwushuang@163.com>
2017-08-25 08:52:13 -0700

    add zip command

---
+++ shell/zip.sh
@@ -0,0 +1,12 @@
+# File Name:    zip.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Wed 26 Jul 2017 08:20:37 AM PDT
+
+#!/bin/bash
+
+# zip is a general tool for file compression
+# A  companion  program (unzip) unpacks zip archives
+
+# No.1 list files within the zip file without decompression
+zip -sf test.zip
