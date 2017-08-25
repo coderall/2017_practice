dcfb107bf7caac19df3c1c28d87dc8c2995c4c5a
alanzfduan <lzcgwushuang@163.com>
2017-08-25 08:51:58 -0700

    add locale command

---
+++ shell/locale.sh
@@ -0,0 +1,14 @@
+# File Name:    locale.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+# Created Time: Wed 26 Jul 2017 10:01:31 AM PDT
+
+#!/bin/bash
+
+# locale get the system character set
+
+# No.1 get the current character encoding set
+locale
+
+# No.2 get all the character set that system support
+locale -a
