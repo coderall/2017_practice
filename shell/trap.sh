4f486ee1b704db17a0b104b4cbb6f0790b9535a0
alanzfduan <lzcgwushuang@163.com>
2017-08-28 08:43:59 +0800

    add trap command detail

--- shell/trap.sh
+++ shell/trap.sh
@@ -4,5 +4,16 @@
 # Created Time: Thu 13 Apr 2017 12:09:49 PM PDT
 
 #!/bin/bash
+
+# trap is a builtin command of bash can deal with signals
 trap "echo get SIGINT" SIGINT 
 sleep 5
+
+# No.2 ignore one specific signal
+# linux shell rely on the tty session concept if user logout the session is gone meanwhile system will 
+# send a SIGHUP to the process.The default signal handler of SIGHUP is to terminal the process
+
+# if you login using ssh and want to backgroud-shell execute normally , you may need to use trap builtin command
+trap "" SIGHUP
+
+
