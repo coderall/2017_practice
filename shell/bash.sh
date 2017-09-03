35d94436af384f8046b30928bf9074fc73577979
alanzfduan <lzcgwushuang@163.com>
2017-09-03 10:32:53 +0800

    add exec file mannually

--- shell/bash.sh
+++ shell/bash.sh
@@ -5,3 +5,8 @@
 # No.1 check the script syntax
 
 bash -x (some shell script)
+
+# No.2 exec some shell
+bash some_shell.sh
+
+# this is very useful to exec some shell mannually with some mount file system without exec option
