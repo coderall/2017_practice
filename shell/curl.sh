634a99b67f84f5ce73ea4968fea77fd6892a2021
alan <lzcgwushuang@163.com>
2017-12-19 19:21:05 +0800

    add curl option explanation
    add --trace option explanation

--- shell/curl.sh
+++ shell/curl.sh
@@ -4,3 +4,6 @@
 
 # Note.1 get the http response header only
 curl -I http://www.baidu.com
+
+# Note.2 dump the request detail to a file,you can see the raw http data including request header & body and response header & body
+curl -X POST http://test.com --trace trace.txt
