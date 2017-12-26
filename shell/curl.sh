a4a82dc8a0cf20c67ae0c0c0bab6bed269f9abd6
alan <lzcgwushuang@163.com>
2017-12-26 11:00:18 +0800

    add curl option
    continue download progress from the last break point

--- shell/curl.sh
+++ shell/curl.sh
@@ -7,3 +7,6 @@ curl -I http://www.baidu.com
 
 # Note.2 dump the request detail to a file,you can see the raw http data including request header & body and response header & body
 curl -X POST http://test.com --trace trace.txt
+
+# Note.3 continue the download from last break point with timeout
+curl -C - http://url -o your_file_name -m 300
