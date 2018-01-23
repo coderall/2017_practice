0c989d125b1fbe63c3bf3c8fbb535e285770dc0e
coderall <lzcgwushuang@163.com>
2018-01-23 19:04:03 +0800

    add shell options

--- shell/builtin.sh
+++ shell/builtin.sh
@@ -2,5 +2,8 @@
 
 # Note this shell shows some shell builtin command usage and options
 
-# <<< this operator redirect the word to the stdin of the command
+# Note.1 <<< this operator redirect the word to the stdin of the command
 md5sum <<< 'hello world'
+
+# Note.2 output continous result
+echo {1..5}
