c5fd747c5df451468f9bab7304be6b8488222ab8
alan <lzcgwushuang@163.com>
2018-03-23 15:57:24 +0800

    add some usage options

--- shell/grep.sh
+++ shell/grep.sh
@@ -27,3 +27,9 @@ echo '123456' | grep -E '[0-9]+' -o   # -o means Print  only  the matched (non-e
 
 # No.7 get the invert match (do not match the candidate word)
 grep -v word
+
+# No.8 match the word in some dir recursively
+grep -R 'some word' some_dir
+
+# No.9 get the result with match line number,it's useful to search some code from big project like kernel or mysql source code
+grep -n -R 'some word' some_dir
