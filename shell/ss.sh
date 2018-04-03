64515957a33ea05a59151ebf190c76ac56761fde
alan <lzcgwushuang@163.com>
2018-04-03 17:49:01 +0800

    add ss command usage

--- shell/ss.sh
+++ shell/ss.sh
@@ -5,3 +5,7 @@
 # No.1 output the network total information (the same with netstat -s)
 
 ss -s
+
+# No.2 show the tcp total num group by all different status
+
+ss -ant | awk -F ' ' '!/State/ {++S[$1]} END { for(a in S) print(a,S[a])}'
