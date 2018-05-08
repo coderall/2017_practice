7c1005a372e4651876978250020031f785717585
alan <lzcgwushuang@163.com>
2018-05-08 17:18:37 +0800

    add ps command usage

--- shell/ps.sh
+++ shell/ps.sh
@@ -3,3 +3,10 @@
 # Note.1 show the mem usage of one process
 ps -o vsz,rss,tsiz,dsiz,majflt,minflt,pmem -p `pidof some_process`
 # vsz=tsiz+dsiz;majflt means some page fault reading data from disk;minflt means some page fault handled by kernel and allocate space 
+
+# Note.2 show the thread id
+ps -eLF
+
+# Note.3 show the column name
+ps -eLF | egrep 'PID|some_executable_name'
+# if you don't use egrep to filter the column name,the result is difficult to see,you don't know the what the value represent
