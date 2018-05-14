23709d689cf16da5a4f10756601411bf9f1e935c
alan <lzcgwushuang@163.com>
2018-05-14 16:25:58 +0800

    add top command

---
+++ shell/top.sh
@@ -0,0 +1,21 @@
+/*************************************************************************
+# File Name:    top.sh
+# Author:     alan
+# mail:       lzcgwushuang@163.com
+ ************************************************************************/
+ 
+ # No.1 top shows all the process stats
+ top
+ 
+ # No.2 sort the result by mem usage
+ top \n & M
+ 
+ # No.3 sort the result by CPU usage
+ top \n & P
+ 
+ # No.4 show the global mem usage
+ top \n & m
+ 
+ # No.5 show all the CPU usage
+ top \n & 1
+ 
