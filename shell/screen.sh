c647e83e084034f703d2dfb34aa9b095f48512f9
alan <lzcgwushuang@163.com>
2018-07-21 23:10:15 +0800

    add screen command

---
+++ shell/screen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# screen is a remote session manage software,can let you keep away from SIGHUP
+
+# No.1 attach a screen session
+screen -x [session name]
+
+# No.2 list all the session
+screen -list 
