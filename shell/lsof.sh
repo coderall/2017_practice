dc980e4cc4dd31596c42a66c04c6bd3ecd68b237
aryacui <aryacui@wesure.cn>
2018-09-16 18:16:58 +0800

    add show regular open file and show ipv4 open file options

--- shell/lsof.sh
+++ shell/lsof.sh
@@ -2,3 +2,9 @@
 # Note.1 check all the open file
 lsof
 # please focus on the file type and the status flag
+
+# No.1 check only regular file
+lsof -p [pid] -a /
+
+# No.2 check ipv4 open file only
+lsof -p [pid] -a -i4
