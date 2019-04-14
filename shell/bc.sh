c92f8229af734d6f6a733da4c614900d4eb6d5b9
coderall <lzcgwushuang@163.com>
2019-04-14 21:59:07 +0800

    add some useful command line

---
+++ shell/bc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# bc is a command line calculator
+
+# No.1
+echo "3*3" | bc
+
+# No.2 float number calculation
+echo "3.0*3.0" | bc -l
--- shell/ss.sh
+++ shell/ss.sh
@@ -9,3 +9,9 @@ ss -s
 # No.2 show the tcp total num group by all different status
 
 ss -ant | awk -F ' ' '!/State/ {++S[$1]} END { for(a in S) print(a,S[a])}'
+
+# No.3 show specific state socket
+ss state time-wait
+
+# No.4 show specific socket detailed information
+ss --info sport = :8080 dport = :8080
